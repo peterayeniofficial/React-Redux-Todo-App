b5fee6e255cfc6d3f8c96bb379afc5dd89ac0c95
Peter Ayeni <ayenicology@gmail.com>
2021-02-21 20:16:54 +0000

    traditional redux patterns

--- src/exampleAddons/middleware.js
+++ src/exampleAddons/middleware.js
@@ -1,3 +1,4 @@
+
 export const print1 = (storeAPI) => (next) => (action) => {
   console.log('1')
   return next(action)
@@ -10,5 +11,33 @@ export const print2 = (storeAPI) => (next) => (action) => {
 
 export const print3 = (storeAPI) => (next) => (action) => {
   console.log('3')
+  return next(action)
+}
+
+export const loggerMiddleware = storeAPI => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', storeAPI.getState())
+  return result
+}
+
+export const delayedMessageMiddleware = storeAPI => next => action => {
+  if (action.type === 'todos/todoAdded') {
+    setTimeout(() => {
+      console.log('Added a new todo: ', action.payload)
+    }, 1000)
+  }
+
+  return next(action)
+}
+
+export const asyncFunctionMiddleware = storeAPI => next => action => {
+  // If the "action" is actually a function instead...
+  if (typeof action === 'function') {
+    // then call the function and pass `dispatch` and `getState` as arguments
+    return action(storeAPI.dispatch, storeAPI.getState)
+  }
+
+  // Otherwise, it's a normal action - send it onwards
   return next(action)
 }
